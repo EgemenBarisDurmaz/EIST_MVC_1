d0cfbf1b18725f2b7831e5462a9892fe16a4b7f1
Egemen Baris Durmaz <ge85vet@mytum.de>
2023-06-13 16:18:35 +0200

    todo 3

--- src/de/tum/in/ase/pse/view/CourseDetailView.java
+++ src/de/tum/in/ase/pse/view/CourseDetailView.java
@@ -29,8 +29,9 @@ public class CourseDetailView extends Stage implements Observer {
 		this.course = course;
 		this.idTextField = new TextField(course.getId());
 		this.nameTextField = new TextField(course.getName());
-
+		this.controller = controller;
 		generateUserInterface();
+		course.notifyObservers();
 	}
 
 	// TODO: Implement save(). This method should update the course with the information entered by the user and save it using the controller
